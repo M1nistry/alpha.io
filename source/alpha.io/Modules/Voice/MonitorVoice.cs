dea9debc6f4523aecb7a337eee1904499a8993e2
Beau <Ministry@live.com.au>
2017-05-25 17:06:46 +1000

    adding export command

--- source/alpha.io/Modules/Voice/MonitorVoice.cs
+++ source/alpha.io/Modules/Voice/MonitorVoice.cs
@@ -21,5 +21,18 @@ namespace alpha.io.Modules.Voice
         {
             
         }
+
+        [Command("export")]
+        [Summary("exports voice data, syntax 'export DD/MM/YYYY-DD/MM/YYYY' date range")]
+        public async Task Export()
+        {
+            var application = await Context.Client.GetApplicationInfoAsync();
+            await ReplyAsync($"You can invite me to your server here: <https://discordapp.com/oauth2/authorize?client_id={application.Id}&scope=bot> (Requires 'Manage Server' permission)");
+        }
+
+        public async Task CreateExport()
+        {
+            //await _db.VoiceActivity.T
+        }
     }
 }
